8c26d36344862608376edf7fb640e85ac75b8cfb
Andrew Ekstedt <andrew.ekstedt@gmail.com>
2011-08-01 23:22:23 -0700

    Add base64 encoding/decoding.

--- jwt.py
+++ jwt.py
@@ -1,4 +1,47 @@
-"""JSON Web Token"""
+u"""JSON Web Token"""
+
+import base64
+import re
+
+def b64e(b):
+    u"""Base64 encode some bytes.
+
+    Uses the url-safe - and _ characters, and doesn't pad with = characters."""
+    return base64.urlsafe_b64encode(b).rstrip(b"=")
+
+_b64_re = re.compile(b"^[A-Za-z0-9_-]*$")
+def b64d(b):
+    u"""Decode some base64-encoded bytes.
+
+    Raises ValueError if the string contains invalid characters or padding."""
+
+    # Python's base64 functions ignore invalid characters, so we need to
+    # check for them explicitly.
+    if not _b64_re.match(b):
+        raise ValueError(b)
+
+    # add padding chars
+    m = len(b) % 4
+    if m == 1:
+        raise ValueError(b)
+    elif m == 2:
+        b += b"=="
+    elif m == 3:
+        b += b"="
+    return base64.urlsafe_b64decode(b)
+
+def split_token(token):
+    l = token.split(b".")
+    if len(l) != 3:
+        raise ValueError
+    return tuple(l)
 
 def check(token):
-    return False
+    if isinstance(token, unicode):
+        raise TypeError
+
+    header, claim, crypto = split_token(token)
+
+    header = b64d(header)
+    claim = b64d(claim)
+    crypto = b64d(crypto)
--- tests.py
+++ tests.py
@@ -1,6 +1,31 @@
 """Tests for the jwt module."""
 
+from nose.tools import assert_raises
+
 import jwt
 
 def test():
-    assert jwt.check("") == False
+    assert_raises(TypeError, lambda: jwt.check(u"unicode string"))
+
+    # too few/many dots
+    assert_raises(ValueError, lambda: jwt.check(b""))
+    assert_raises(ValueError, lambda: jwt.check(b"a.b"))
+    assert_raises(ValueError, lambda: jwt.check(b"a.b.c.d"))
+
+    jwt.check(b"..")
+
+    # invalid base64 encoding
+    assert_raises(ValueError, lambda: jwt.check(b"!!.$$.&&"))
+
+    jwt.check(b"AAAA.AAA.AA")
+
+def test_b64d():
+    # invalid chars
+    assert_raises(ValueError, lambda: jwt.b64d(b"!!!!"))
+    assert_raises(ValueError, lambda: jwt.b64d(b" AA A A"))
+
+    # padding present
+    assert_raises(ValueError, lambda: jwt.b64d(b"AA=="))
+
+    # invalid length
+    assert_raises(ValueError, lambda: jwt.b64d(b"A"))
